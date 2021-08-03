d7b2b30ff269627dfdacc948263562afe56627e5
Nick Girardo <nickgirardo@gmail.com>
2021-08-03 15:52:18 -0400

    Change buttons to divs for focus behavior

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,10 +13,17 @@ export const Button = ({ active, label, onClick }: Props) => {
     `button-${label.toLowerCase()}`,
   );
 
+  // NOTE using a div here instead of a button because the default button
+  // behavior wrt focus messes with using space to change between entry modes
+  // This is obviously bad for accessibility
+  // TODO improve accessibility here (aria or find alt fix for button focus)
   return (
-    <button className={ classes } onClick={ onClick }>
+    <div
+      className={ classes }
+      onClick={ onClick }
+    >
       { label }
-    </button>
+    </div>
   );
 }
 
