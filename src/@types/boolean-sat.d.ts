13845a8e0cac9044b2e75300f69257cbea02a96e
Nick Girardo <nickgirardo@gmail.com>
2021-08-10 00:23:57 -0400

    Types: Fixed type for boolean-sat result

--- src/@types/boolean-sat.d.ts
+++ src/@types/boolean-sat.d.ts
@@ -1,7 +1,7 @@
 declare module 'boolean-sat' {
   // Note that as the first variable (0) is unused (it cannot be negated)
   // so the first element in the array is always null
-  export type SATSolution = [null, ...number[]];
+  export type SATSolution = [null, ...boolean[]];
 
   // Returns a solution if satisfiable or false if unsatisfiable
   function satSolve(
