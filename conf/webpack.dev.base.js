bb9ddb0d72939ed34c21cf04f10a620e01678462
Nick Girardo <nickgirardo@gmail.com>
2024-02-07 18:00:05 -0500

    Change output directory to dist/

--- conf/webpack.dev.base.js
+++ conf/webpack.dev.base.js
@@ -1,40 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   output: {
-    path: path.resolve(__dirname, '../build'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].js",
   },
   resolve: {
-    extensions: ['', '.js', '.ts', '.jsx', '.tsx'],
+    extensions: ["", ".js", ".ts", ".jsx", ".tsx"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
-	/*
-  devServer: {
-    contentBase: path.resolve(__dirname, './build'),
-  },
-  */
   watchOptions: {
     ignored: /node_modules/,
   },
-}
-
+};
--- conf/webpack.prod.base.js
+++ conf/webpack.prod.base.js
@@ -1,37 +1,32 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  devtool: 'inline-source-map',
+  mode: "production",
+  devtool: "inline-source-map",
   output: {
     // NOTE for some reason this dir seems to be based on the location of this file
     // all other paths seem to be based on the repo root
-    path: path.resolve(__dirname, '../build'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].js",
   },
   resolve: {
-    extensions: ['', '.js', '.ts', '.jsx', '.tsx'],
+    extensions: ["", ".js", ".ts", ".jsx", ".tsx"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
   optimization: {
     minimize: true,
   },
-}
-
+};
